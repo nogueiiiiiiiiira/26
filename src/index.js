60409b20b1ee81c706cee92329cfb890b23a32d3
nogueiiiiiiiiira <karencristininogueira@gmail.com>
2024-04-26 17:05:55 -0300

    React

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Navbar, Footer } from './pages/layout';
+import { Home } from './pages/home';
+import { Products } from './pages/products';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return(
+    <div>
+      <>
+      <BrowserRouter>
+       <Navbar />
+       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+       </Routes>
+       <Footer />
+      </BrowserRouter>
+
+      </>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
